8e08f62d873c615c99c500448fdd408cdfeed0c6
gopalraj321 <gopalraj321@gmail.com>
2025-05-26 11:14:39 +0000

    adding java project

---
+++ myjava/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins CI/CD with Maven & SonarQube!");
+    }
+
+    public static String greet() {
+        return "Hello from greet() method!";
+    }
+}
